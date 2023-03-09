1fb049097858c361c163a9aa8b553ab37d7f24b7
Patryk Młyńczak <patryk.mlynczak@fingerprints.digital>
2023-03-09 23:16:57 +0100

    wisielec zrobiony

--- wisielec.py
+++ wisielec.py
@@ -15,7 +15,8 @@ user_word = []
 used_letters = []
 for _ in word:
     user_word.append("_")
-while True:
+run = True
+while run:
     user_letter = input("Podaj literę: ").upper()
     used_letters.append(user_letter)
     index = -1
@@ -27,17 +28,20 @@ while True:
             letter_hit = True
 
     if not letter_hit:
-
-        if fail_counter == no_of_tries:
-            print("Gra skończona")
-            break
         fail_counter += 1
         i += 1
         x = 0
         for number in range(0, i, 1):
             print(wisielec[7 - i + x])
             x += 1
-
+        if fail_counter == no_of_tries:
+            print("Gra skończona")
+            break
     else:
         print("Trafiłeś litery : ")
         print(user_word)
+
+    if list(word) == user_word:
+        print("Gratulacje, odgadnąłeś hasło !")
+        break
+
