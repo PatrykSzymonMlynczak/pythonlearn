b488e1f8017f47288dbb67f786d6eba451363511
Patryk Młyńczak <patryk.mlynczak@fingerprints.digital>
2023-03-09 22:44:47 +0100

    rysowanie wisielca ze stringa

--- wisielec.py
+++ wisielec.py
@@ -1,5 +1,43 @@
+wisielec = """
+           ______________
+           |            |
+           |            0
+           |           /|\\
+           |           /\\
+      _____|_____
+""".splitlines()
+
 no_of_tries = 5
-word = "Patryk"
+fail_counter = -1
+i = 0
+word = "Patryk".upper()
 user_word = []
+used_letters = []
+for _ in word:
+    user_word.append("_")
 while True:
-    letter = input("Podaj literę: ")
+    user_letter = input("Podaj literę: ").upper()
+    used_letters.append(user_letter)
+    index = -1
+    letter_hit = False
+    for letter in word:
+        index += 1
+        if letter == user_letter:
+            user_word[index] = word[index]
+            letter_hit = True
+
+    if not letter_hit:
+
+        if fail_counter == no_of_tries:
+            print("Gra skończona")
+            break
+        fail_counter += 1
+        i += 1
+        x = 0
+        for number in range(0, i, 1):
+            print(wisielec[7 - i + x])
+            x += 1
+
+    else:
+        print("Trafiłeś litery : ")
+        print(user_word)
